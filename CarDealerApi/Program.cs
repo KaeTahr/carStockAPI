750b4fcc6fbf822ff9fe093c4724e5b5bdbb3f8b
kaetahr <lostech@protonmail.com>
2026-03-26 17:42:45 +1100

    login and register frontend

--- CarDealerApi/Program.cs
+++ CarDealerApi/Program.cs
@@ -5,6 +5,7 @@ using Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddFastEndpoints();
 // JWT
 builder.Services.AddAuthenticationJwtBearer(s =>
 {
@@ -12,22 +13,35 @@ builder.Services.AddAuthenticationJwtBearer(s =>
 });
 builder.Services.AddAuthorization();
 
-builder.Services.AddFastEndpoints();
 builder.Services.SwaggerDocument(o =>
 {
     o.ExcludeNonFastEndpoints = true;
     o.DocumentSettings = s => s.EnableJWTBearerAuth();
 });
 
-
 builder.Services.AddSingleton<DbConnectionFactory>();
 builder.Services.AddSingleton<Data.DbInitializer>(); // init and seed DB
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseFastEndpoints();
+app.UseFastEndpoints(c =>
+    {
+        c.Errors.StatusCode = 400;
+    }
+);
 app.UseSwaggerGen();
 
 // running DB initializer to create tables and seed data
